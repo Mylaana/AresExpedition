b6af98b389646aec5c9f431e4cbcf0c17563bbae
alex <alexandre.thore34@gmail.com>
2024-07-06 23:55:24 +0200

    parser done

--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import json
+import copy
 
 
 parser_columns_map = [
@@ -24,20 +25,18 @@ parser_columns_map = [
     {'column_name': 'stockableRessource', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
     {'column_name': 'title_en', 'column_id': -1 , 'output_field_name': 'title', 'split_per_language': True},
     {'column_name': 'title_fr', 'column_id': -1 , 'output_field_name': 'title', 'split_per_language': True},
-    {'column_name': 'description_en', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'description_fr', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'vpText_en', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'vpText_fr', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'effectSummaryText_en', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'effectSummaryText_fr', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'effectText_en', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'effectText_fr', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
+    {'column_name': 'vpText_en', 'column_id': -1 , 'output_field_name': 'vpText', 'split_per_language': True},
+    {'column_name': 'vpText_fr', 'column_id': -1 , 'output_field_name': 'vpText', 'split_per_language': True},
+    {'column_name': 'effectSummaryText_en', 'column_id': -1 , 'output_field_name': 'effectSummaryText', 'split_per_language': True},
+    {'column_name': 'effectSummaryText_fr', 'column_id': -1 , 'output_field_name': 'effectSummaryText', 'split_per_language': True},
+    {'column_name': 'effectText_en', 'column_id': -1 , 'output_field_name': 'effectText', 'split_per_language': True},
+    {'column_name': 'effectText_fr', 'column_id': -1 , 'output_field_name': 'effectText', 'split_per_language': True},
     {'column_name': 'playedText_en', 'column_id': -1 , 'output_field_name': 'playedText', 'split_per_language': True},
     {'column_name': 'playedText_fr', 'column_id': -1 , 'output_field_name': 'playedText', 'split_per_language': True},
-    {'column_name': 'prerequisiteText_en', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'prerequisiteText_fr', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'descriptionPrerequisiteSummary_en', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False},
-    {'column_name': 'descriptionPrerequisiteSummary_fr', 'column_id': -1 , 'output_field_name': '', 'split_per_language': False}
+    {'column_name': 'prerequisiteText_en', 'column_id': -1 , 'output_field_name': 'prerequisiteText', 'split_per_language': True},
+    {'column_name': 'prerequisiteText_fr', 'column_id': -1 , 'output_field_name': 'prerequisiteText', 'split_per_language': True},
+    {'column_name': 'prerequisiteSummaryText_en', 'column_id': -1 , 'output_field_name': 'prerequisiteSummaryText', 'split_per_language': True},
+    {'column_name': 'prerequisiteSummaryText_fr', 'column_id': -1 , 'output_field_name': 'prerequisiteSummaryText', 'split_per_language': True}
     ]
 
 PARSER_CARD_INFO_MODEL = {
@@ -54,34 +53,13 @@ PARSER_CARD_INFO_MODEL = {
     "prerequisiteTresholdValue": 0,
     "phaseUp": "",
     "phaseDown": "",
-    "title": {
-        "en":"",
-        "fr":""
-    },
-    "playedText":{
-        "en":"",
-        "fr":""
-    },
-    "descriptionPrerequisite": {
-        "en":"",
-        "fr":""
-    },
-    "descriptionPrerequisiteSummary": {
-        "en": "",
-        "fr": ""
-    },
-    "descriptionVp": {
-        "en":"",
-        "fr":""
-    },
-    "descriptionEffect": {
-        "en":"",
-        "fr":""
-    },
-    "descriptionSummary": {
-        "en": "",
-        "fr": ""
-    }
+    "title": {},
+    "vpText": {},
+    "effectSummaryText": {},
+    "effectText": {},
+    "playedText": {},
+    "prerequisiteText": {},
+    "prerequisiteSummaryText": {}
 }
 
 def map_csv_columns(csv_header: str):
@@ -108,8 +86,22 @@ def parse_row(csv_row: str):
             language = map['column_name'].split('_')[1]
             parsed_row[map['output_field_name']][language] = csv_row[map['column_id']]
             continue
+        
+        parsed_value = csv_row[map['column_id']]
 
-        parsed_row[map['output_field_name']] = csv_row[map['column_id']]
+        match str(type(PARSER_CARD_INFO_MODEL[map['output_field_name']])):
+            case "<class 'list'>":
+                parsed_value = parsed_value.split(',')
+                parsed_row[map['output_field_name']] = parsed_value
+                for index in range(len(parsed_value)):
+                    if parsed_value[index] == '':
+                        continue
+                    parsed_value[index] = int(parsed_value[index])
+            case "<class 'int'>":
+                parsed_row[map['output_field_name']] = int(parsed_value)
+            case _:
+                parsed_row[map['output_field_name']] = parsed_value
+
     return parsed_row
 
 
@@ -118,9 +110,7 @@ def main():
     input_name = 'card_list'
     output_path = os.path.dirname(__file__)
     output_name = 'cards_data.json'
-    
     parsed = []
-
     with open(file=input_path + input_name + ".csv", mode="r", encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         skipped_header = False
@@ -130,10 +120,12 @@ def main():
                 map_csv_columns(row)
                 continue
 
-            parsed.append(parse_row(row).copy())
-        
-        with open(os.path.join(output_path, output_name), 'w') as f:
-            json.dump(parsed, f, indent=4)
+            parsed.append(copy.deepcopy(parse_row(row)))
+
+    with open(os.path.join(output_path, output_name), 'w') as f:
+        json.dump(parsed, f, indent=4)
+    
+    print('Done')
 
 if __name__ == '__main__':
     main()
