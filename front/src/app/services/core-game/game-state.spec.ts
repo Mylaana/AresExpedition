6741b3e1c2b291283548001acbcdd9832bc5b71b
Mylaana <alexandre.thore34@gmail.com>
2024-10-23 12:22:18 +0200

    add first test for game state service

--- front/src/app/services/core-game/game-state.spec.ts
+++ front/src/app/services/core-game/game-state.spec.ts
@@ -1,7 +1,22 @@
+import { PhaseCardInfoService } from "../cards/phase-card-info.service"
+import { ProjectCardInfoService } from "../cards/project-card-info.service"
+import { ProjectCardPlayedEffectService } from "../cards/project-card-played-effect.service"
+import { ProjectCardScalingProductionsService } from "../cards/project-card-scaling-productions.service"
+import { GameState } from "./game-state.service"
+
 describe('Services - Core game - Game state', () => {
+    let projectCardService = new ProjectCardInfoService
+    let phaseCardService = new PhaseCardInfoService
+    let scalingProdService = new ProjectCardScalingProductionsService
+    let playedCardService = new ProjectCardPlayedEffectService(scalingProdService)
+    let gameState = new GameState(projectCardService, phaseCardService, playedCardService)
+
     describe('UNIT TEST', () => {
-        it('is fake', () => {
-            expect(1).toBe(1)
+        it('should evaluate getClientPlayerState', () => {
+            let state = gameState.getClientPlayerState()
+            const spy = spyOn(GameState.prototype, 'getPlayerStateFromId')
+            expect(state).toBeUndefined()
+            expect(spy).toHaveBeenCalled
         })
     })
 })
