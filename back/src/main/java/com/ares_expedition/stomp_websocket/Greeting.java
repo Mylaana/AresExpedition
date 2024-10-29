dd922a56ad901cfafea042521f3c42a3fd4ad0a5
Mylaana <alexandre.thore34@gmail.com>
2024-10-29 09:20:33 +0100

    server side examplet websocket working

---
+++ back/src/main/java/com/ares_expedition/stomp_websocket/Greeting.java
@@ -0,0 +1,18 @@
+package com.ares_expedition.stomp_websocket;
+
+public class Greeting {
+
+    private String content;
+  
+    public Greeting() {
+    }
+  
+    public Greeting(String content) {
+      this.content = content;
+    }
+  
+    public String getContent() {
+      return content;
+    }
+  
+  }
