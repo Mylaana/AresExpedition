e68d8ab2677ef69769568d9325a6aec0d7418074
Mylaana <alexandre.thore34@gmail.com>
2024-11-29 14:52:58 +0100

    fixed test

--- front/src/app/services/core-game/game-state.spec.ts
+++ front/src/app/services/core-game/game-state.spec.ts
@@ -2,6 +2,7 @@ import { PhaseCardInfoService } from "../cards/phase-card-info.service"
 import { ProjectCardInfoService } from "../cards/project-card-info.service"
 import { ProjectCardPlayedEffectService } from "../cards/project-card-played-effect.service"
 import { ProjectCardScalingProductionsService } from "../cards/project-card-scaling-productions.service"
+import { RxStompService } from "../websocket/rx-stomp.service"
 import { GameState } from "./game-state.service"
 
 describe('Services - Core game - Game state', () => {
@@ -9,7 +10,8 @@ describe('Services - Core game - Game state', () => {
     let phaseCardService = new PhaseCardInfoService
     let scalingProdService = new ProjectCardScalingProductionsService
     let playedCardService = new ProjectCardPlayedEffectService(scalingProdService)
-    let gameState = new GameState(projectCardService, phaseCardService, playedCardService)
+    let rxStompService = new RxStompService
+    let gameState = new GameState(projectCardService, phaseCardService, playedCardService, rxStompService)
 
     describe('UNIT TEST', () => {
         it('should evaluate getClientPlayerState', () => {
