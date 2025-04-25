1f604fce7352a1f9729297fb0999148fe4d8ece0
Mylaana <alexandre.thore34@gmail.com>
2025-04-25 12:08:33 +0200

    added dedicated vp shape component, updated cards and global pannel

--- front/src/app/components/player-info/global-pannel/global-pannel.component.ts
+++ front/src/app/components/player-info/global-pannel/global-pannel.component.ts
@@ -4,10 +4,14 @@ import { PlayerStateModel } from '../../../models/player-info/player-state.model
 import { PlayerPhase } from '../../../interfaces/global.interface';
 import { NonSelectablePhaseEnum } from '../../../enum/phase.enum';
 import { myUUID } from '../../../types/global.type';
+import { VpComponent } from '../../tools/vp/vp.component';
 
 @Component({
     selector: 'app-global-pannel',
-    imports: [CommonModule],
+    imports: [
+      CommonModule,
+      VpComponent
+    ],
     templateUrl: './global-pannel.component.html',
     styleUrl: './global-pannel.component.scss'
 })
---
+++ front/src/app/components/tools/vp/vp.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-vp',
+  imports: [CommonModule],
+  templateUrl: './vp.component.html',
+  styleUrl: './vp.component.scss'
+})
+export class VpComponent {
+	@Input() vpNumber!: string
+  @Input() shape: 'round' | 'square' = 'round'
+}
