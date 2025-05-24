b0203310b48b6c17f80d111730fc1e72bf10e22e
Mylaana <alexandre.thore34@gmail.com>
2025-05-24 12:35:12 +0200

    fine tuning

--- front/src/app/components/cards/project/card-blocks/card-status/card-status.component.ts
+++ front/src/app/components/cards/project/card-blocks/card-status/card-status.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { fadeInFadeOut } from '../../../../../animations/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-status',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-status.component.html',
   styleUrl: './card-status.component.scss'
 })
 export class CardStatusComponent {
 	@Input() cardCode!: string
 	@Input() cardStatus!: string
+	@Input() hovered: boolean = false
 }
