90c6f9da3a98f3e0ab84bc6f94176f49d6dbd20a
Mylaana <alexandre.thore34@gmail.com>
2025-06-03 11:25:36 +0200

    captions

--- front/src/app/services/cards/project-card-scaling-VP.service.ts
+++ front/src/app/services/cards/project-card-scaling-VP.service.ts
@@ -9,24 +9,25 @@ import { PlayableCardModel } from "../../models/cards/project-card.model";
 export class ProjectCardScalingVPService {
 	scaledVpList: CardScalingVP[]= []
 	private readonly vpCalculators: Record<string, (card: PlayableCardModel, state: PlayerStateModel) => number> = {
-		"12": (card) => card.getStockValue('animal'), // Birds
+		"12": (card) => card.getStockValue('animal'), //Birds
 		"18": (_, state) => Math.floor(state.getForest() / 2),
-		"30": (card) => card.getStockValue('animal'), // Fish
-		"33": (card) => Math.floor(card.getStockValue('animal') / 2), // Herbivore
+		"30": (card) => card.getStockValue('animal'), //Fish
+		"33": (card) => Math.floor(card.getStockValue('animal') / 2), //Herbivore
+		//Interplanetary Relations
 		"35": (_, state) => {
 			const valid = state.getProjectPlayedModelList()
 				.filter(c => c.cardType !== 'corporation').length;
 			return Math.floor(valid / 4);
 		},
-		"39": (card) => card.getStockValue('animal'), // Livestock
+		"39": (card) => card.getStockValue('animal'), //Livestock
 		"46": (card) => Math.floor(card.getStockValue('science') / 2),
-		"53": (card) => Math.floor(card.getStockValue('animal') / 2), // Small Animals
-		"58": (card) => Math.floor(card.getStockValue('microbe') / 3),
-		"59": (_, state) => Math.floor(state.getProjectPlayedIdList({ type: 'blueProject' }).length / 3),
-		"63": (_, state) => state.getTagsOfType('jovian'),
-		"153": (_, state) => state.getTagsOfType('jovian'),
-		"F07": (card) => Math.floor(card.getStockValue('animal') / 2), // Pets
-		"CP01": (card) => Math.floor(card.getStockValue('animal') / 2), // Arklight
+		"53": (card) => Math.floor(card.getStockValue('animal') / 2), //Small Animals
+		"58": (card) => Math.floor(card.getStockValue('microbe') / 3), //Tardigrades
+		"59": (_, state) => Math.floor(state.getProjectPlayedIdList({ type: 'blueProject' }).length / 3), //Think Tank
+		"63": (_, state) => state.getTagsOfType('jovian'), //Water Import from Europa
+		"153": (_, state) => state.getTagsOfType('jovian'), //Io Mining Industries
+		"F07": (card) => Math.floor(card.getStockValue('animal') / 2), //Pets
+		"CP01": (card) => Math.floor(card.getStockValue('animal') / 2), //Arklight
 	};
 
 	updateCardScalingVPList(clientState: PlayerStateModel) {
