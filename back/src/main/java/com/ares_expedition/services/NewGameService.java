95551919088cdc663ec2ef2e665928e82d00c968
Mylaana <alexandre.thore34@gmail.com>
2025-07-27 20:33:30 +0200

    logging cleaning, fixed exocorp v2

--- back/src/main/java/com/ares_expedition/services/NewGameService.java
+++ back/src/main/java/com/ares_expedition/services/NewGameService.java
@@ -1,7 +1,10 @@
 package com.ares_expedition.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.ares_expedition.controller.websocket.InputRouter;
 import com.ares_expedition.dto.api.NewGameConfigDTO;
 import com.ares_expedition.dto.api.NewGameInfoDTO;
 import com.ares_expedition.model.core.Game;
@@ -9,10 +12,11 @@ import com.ares_expedition.repository.JsonGameDataHandler;
 
 @Service
 public class NewGameService {
+    private static final Logger logger = LoggerFactory.getLogger(InputRouter.class);
     public NewGameInfoDTO createGame(NewGameConfigDTO gameConfig) {
         Game game = Game.createGame(gameConfig);
         this.addNewGameToDataBase(game);
-
+        logger.warn("\u001B[32m Creating new game with id: " + gameConfig.getGameId() + " for : " + gameConfig.getPlayers().size()+ " player(s) \u001B[0m");
         return new NewGameInfoDTO(gameConfig.getGameId(), gameConfig.getPlayers(), gameConfig.getOptions());
     }
     private void addNewGameToDataBase(Game newGame) {
