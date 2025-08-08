d7e109c19a6eee9334953e801931082da9cc953d
Mylaana <alexandre.thore34@gmail.com>
2025-08-08 14:00:15 +0200

    cleaning

--- back/src/main/java/com/ares_expedition/services/NewGameService.java
+++ back/src/main/java/com/ares_expedition/services/NewGameService.java
@@ -1,7 +1,5 @@
 package com.ares_expedition.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.ares_expedition.dto.api.NewGameConfigDTO;
