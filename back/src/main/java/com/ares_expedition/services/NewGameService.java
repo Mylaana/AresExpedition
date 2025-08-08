2600e15fcf77a6ea839c7c59b293637762d5a92f
Mylaana <alexandre.thore34@gmail.com>
2025-08-08 13:59:54 +0200

    removed logger from service

--- back/src/main/java/com/ares_expedition/services/NewGameService.java
+++ back/src/main/java/com/ares_expedition/services/NewGameService.java
@@ -11,11 +11,9 @@ import com.ares_expedition.repository.JsonGameDataHandler;
 
 @Service
 public class NewGameService {
-    private static final Logger logger = LoggerFactory.getLogger(NewGameService.class);
     public NewGameInfoDTO createGame(NewGameConfigDTO gameConfig) {
         Game game = Game.createGame(gameConfig);
         this.addNewGameToDataBase(game);
-        logger.warn("\u001B[32m Creating new game with id: " + gameConfig.getGameId() + " for : " + gameConfig.getPlayers().size()+ " player(s) \u001B[0m");
         return new NewGameInfoDTO(gameConfig.getGameId(), gameConfig.getPlayers(), gameConfig.getOptions());
     }
     private void addNewGameToDataBase(Game newGame) {
