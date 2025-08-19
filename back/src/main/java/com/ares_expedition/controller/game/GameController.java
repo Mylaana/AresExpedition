09105151b1f3c0c62ab731116087d0e01cbb5ae3
Mylaana <alexandre.thore34@gmail.com>
2025-08-19 15:03:02 +0200

    fix log

--- back/src/main/java/com/ares_expedition/controller/game/GameController.java
+++ back/src/main/java/com/ares_expedition/controller/game/GameController.java
@@ -95,10 +95,10 @@ public class GameController {
         game.fillDiscardPileFromPlayerDiscard();
         game.resetResearchResolved();
         game.claimMilestones();
-        logger.warn("\u001B[32m ------------Game Over------------ \u001B[0m");
-        logger.warn("id: " + game.getGameId());
-        logger.warn("\u001B[32m -------------------------------- \u001B[0m");
         if(game.isGameOver()){
+            logger.warn("\u001B[32m ------------Game Over------------ \u001B[0m");
+            logger.warn("id: " + game.getGameId());
+            logger.warn("\u001B[32m -------------------------------- \u001B[0m");
             game.setGameStatus(GameStatusEnum.GAME_OVER);
             wsOutput.sendPushToGroup(MessageOutputFactory.createNextPhaseMessage(game.getGameId(), game.getGameState()));
             return;
