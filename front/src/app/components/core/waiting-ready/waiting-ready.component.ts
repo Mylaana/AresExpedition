8a82452c712245f7ddd88a368d04e2ba2556db81
Mylaana <alexandre.thore34@gmail.com>
2025-08-25 15:41:28 +0200

    visually improved waiting player screens

--- front/src/app/components/core/waiting-ready/waiting-ready.component.ts
+++ front/src/app/components/core/waiting-ready/waiting-ready.component.ts
@@ -7,12 +7,15 @@ import { PlayerReadyModel, PlayerStateModel } from '../../../models/player-info/
 import { fadeIn } from '../../../animations/animations';
 import { PlayableCardModel } from '../../../models/cards/project-card.model';
 import { PlayableCardListComponent } from '../../cards/project/playable-card-list/playable-card-list.component';
+import { PlayerNameComponent } from '../../player-info/player-name/player-name.component';
+import { PlayerInfoStateModel } from '../../../models/player-info/player-state-info.model';
 
 @Component({
     selector: 'app-waiting-ready',
     imports: [
         CommonModule,
-		PlayableCardListComponent
+		PlayableCardListComponent,
+		PlayerNameComponent
     ],
     templateUrl: './waiting-ready.component.html',
     styleUrl: './waiting-ready.component.scss',
@@ -25,10 +28,12 @@ export class WaitingReadyComponent implements OnInit{
 
 	private destroy$ = new Subject<void>()
 	_groupPlayerReady: PlayerReadyModel[] = []
+	_groupInfo!: PlayerInfoStateModel[]
 
 	constructor(private gameStateService: GameState){}
 
 	ngOnInit(): void {
+		this.setGroupInfo(this.gameStateService.getGroupState())
 		this.maxCorporations = this.gameStateService.getGameOptions().merger?2:1
 		this.gameStateService.currentGroupPlayerReady.pipe(takeUntil(this.destroy$)).subscribe(ready => this.onGroupReadyUpdate(ready))
 		this.gameStateService.currentClientState.pipe(takeUntil(this.destroy$)).subscribe(state => this.updateWaitingCorporationList(state))
@@ -47,5 +52,17 @@ export class WaitingReadyComponent implements OnInit{
 			return
 		}
 		this._waitingCorporationList = this.gameStateService.getClientHandCorporationModelList()
+	}
+	private setGroupInfo(groupState: PlayerStateModel[]){
+		this._groupInfo = []
+		for(let s of groupState){
+			this._groupInfo.push(s.getInfoState())
+		}
+	}
+	isReady(playerName: string): boolean {
+		for(let p of this._groupPlayerReady){
+			if(p.name===playerName){return p.isReady}
+		}
+		return false
 	}
 }
--- front/src/app/components/player-info/player-name/player-name.component.ts
+++ front/src/app/components/player-info/player-name/player-name.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { RGB } from '../../../types/global.type';
 import { CommonModule } from '@angular/common';
 
+type PlayerNameStyle = 'default' | 'short'
+
 @Component({
   selector: 'app-player-name',
   imports: [CommonModule],
@@ -11,4 +13,6 @@ import { CommonModule } from '@angular/common';
 export class PlayerNameComponent {
 	@Input() name!: string
 	@Input() color!: RGB
+	@Input() isReady!: boolean
+	@Input() style: PlayerNameStyle = 'default'
 }
