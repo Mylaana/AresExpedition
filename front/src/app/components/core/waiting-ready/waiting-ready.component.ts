5c82cf3d567722cee1a177fe6547ec059fd82754
Mylaana <alexandre.thore34@gmail.com>
2025-08-25 15:09:04 +0200

    added corporation list to pick from while waiting post initial draft

--- front/src/app/components/core/waiting-ready/waiting-ready.component.ts
+++ front/src/app/components/core/waiting-ready/waiting-ready.component.ts
@@ -3,13 +3,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { EventBaseModel } from '../../../models/core-game/event.model';
 import { Subject, takeUntil } from 'rxjs';
 import { GameState } from '../../../services/core-game/game-state.service';
-import { PlayerReadyModel } from '../../../models/player-info/player-state.model';
+import { PlayerReadyModel, PlayerStateModel } from '../../../models/player-info/player-state.model';
 import { fadeIn } from '../../../animations/animations';
+import { PlayableCardModel } from '../../../models/cards/project-card.model';
+import { PlayableCardListComponent } from '../../cards/project/playable-card-list/playable-card-list.component';
 
 @Component({
     selector: 'app-waiting-ready',
     imports: [
-        CommonModule
+        CommonModule,
+		PlayableCardListComponent
     ],
     templateUrl: './waiting-ready.component.html',
     styleUrl: './waiting-ready.component.scss',
@@ -17,6 +20,8 @@ import { fadeIn } from '../../../animations/animations';
 })
 export class WaitingReadyComponent implements OnInit{
 	@Input() event!: EventBaseModel
+	_waitingCorporationList!:PlayableCardModel[]
+	private maxCorporations: number = 0
 
 	private destroy$ = new Subject<void>()
 	_groupPlayerReady: PlayerReadyModel[] = []
@@ -24,7 +29,9 @@ export class WaitingReadyComponent implements OnInit{
 	constructor(private gameStateService: GameState){}
 
 	ngOnInit(): void {
+		this.maxCorporations = this.gameStateService.getGameOptions().merger?2:1
 		this.gameStateService.currentGroupPlayerReady.pipe(takeUntil(this.destroy$)).subscribe(ready => this.onGroupReadyUpdate(ready))
+		this.gameStateService.currentClientState.pipe(takeUntil(this.destroy$)).subscribe(state => this.updateWaitingCorporationList(state))
 	}
 	ngOnDestroy(): void {
 		this.destroy$.next()
@@ -32,5 +39,13 @@ export class WaitingReadyComponent implements OnInit{
 	}
 	onGroupReadyUpdate(groupReady: PlayerReadyModel[]): void {
 		this._groupPlayerReady = groupReady
+	}
+	updateWaitingCorporationList(state: PlayerStateModel){
+		let corporations = state.getPlayedCorporations()
+		if(corporations.length>=this.maxCorporations){
+			this._waitingCorporationList = []
+			return
+		}
+		this._waitingCorporationList = this.gameStateService.getClientHandCorporationModelList()
 	}
 }
