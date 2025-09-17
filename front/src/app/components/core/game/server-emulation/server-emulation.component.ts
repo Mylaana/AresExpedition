e82e1d52a864af955142e23a43e7a489c08b223a
Mylaana <alexandre.thore34@gmail.com>
2025-09-17 11:40:29 +0200

    update

--- front/src/app/components/core/game/server-emulation/server-emulation.component.ts
+++ front/src/app/components/core/game/server-emulation/server-emulation.component.ts
@@ -154,7 +154,7 @@ export class ServerEmulationComponent implements OnInit, AfterViewInit, OnDestro
 	}
 	drawCards(): void {
 		//force draw card list for debug purpose
-		let cardDrawList: string[] = ['FM32', 'FM33', 'FM34', 'FM35', 'D31']
+		let cardDrawList: string[] = []
 
 		this.gameStateService.addCardsToClientHand(cardDrawList)
 		this.gameStateService.updateClientState(this.gameStateService.getClientState())
