6b28471d62d75c0d8adfbc03745a362ee7042fb9
Mylaana <alexandre.thore34@gmail.com>
2025-11-25 13:01:10 +0100

    added new component

---
+++ front/src/app/components/core/post-game/post-game-cards/post-game-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-game-cards',
+  imports: [],
+  templateUrl: './post-game-cards.component.html',
+  styleUrl: './post-game-cards.component.scss'
+})
+export class PostGameCardsComponent {
+
+}
